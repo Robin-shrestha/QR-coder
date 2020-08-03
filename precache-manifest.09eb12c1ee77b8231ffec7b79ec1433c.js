22331baf4ac3d324d8a9be91ae9c391f5d5d0134
Robin-shrestha <rsresthas@gmail.com>
2020-08-03 16:27:00 +0545

    Updates

---
+++ precache-manifest.09eb12c1ee77b8231ffec7b79ec1433c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29780f768e32c33be8b62919f55c2009",
+    "url": "/QR-coder/index.html"
+  },
+  {
+    "revision": "0deb4cd36b1e5c43d864",
+    "url": "/QR-coder/static/css/main.1b34018a.chunk.css"
+  },
+  {
+    "revision": "2ec8689e177e997fdb8e",
+    "url": "/QR-coder/static/js/2.92affbaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QR-coder/static/js/2.92affbaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0deb4cd36b1e5c43d864",
+    "url": "/QR-coder/static/js/main.e4e09e80.chunk.js"
+  },
+  {
+    "revision": "65760038ea3a178a311e",
+    "url": "/QR-coder/static/js/runtime-main.854c909a.js"
+  }
+]);
